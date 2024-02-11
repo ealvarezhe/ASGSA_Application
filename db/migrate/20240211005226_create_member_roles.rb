95fd749e24c47d75834e6012c4d43adaa1bb1be2
revmya09 <113645178+revmya09@users.noreply.github.com>
2024-02-11 17:57:08 +0000

    fixed member_role_id

--- db/migrate/20240211005226_create_member_roles.rb
+++ db/migrate/20240211005226_create_member_roles.rb
@@ -1,7 +1,7 @@
 class CreateMemberRoles < ActiveRecord::Migration[7.1]
   def change
     create_table :member_roles do |t|
-      t.integer :id
+      t.integer :member_role_id
       t.integer :member_id
       t.integer :role_id
 
