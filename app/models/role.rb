ee9a19c2ec8b86b1aeda540709f819a4ceca31c0
revmya09 <113645178+revmya09@users.noreply.github.com>
2024-02-12 17:12:13 +0000

    added input validity for member_role and roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,12 @@
 class Role < ApplicationRecord
     include PgSearch::Model
     pg_search_scope :search, against: [:role_id, :name, :permissions], using: { tsearch: { prefix: true } }
+    has_many :member_roles
+    has_many :members, through: :member_roles
+    validates :name, presence: true
+    validates :permissions, presence: true
+    validates :role_id, presence: true
+    validates :role_id, numericality: { only_integer: true }
+    validates :role_id, uniqueness: true
+    validates :name, presence: true, inclusion: { in: %w(admin member officer), message: "%{value} is not a valid role" }
 end
