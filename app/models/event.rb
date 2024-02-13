ab7d0784bd43c8f14c339aa1206b6eab8bc945f4
Nicholas Nguyen <nick.nguyen1@tamu.edu>
2024-02-12 20:01:22 -0600

    Added tests and validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
-    validates_presence_of :name, :location, :start_time, :end_time, :date, :points
+    validates :name, presence: true
+    validates :location, presence: true
+    validates :start_time, presence: true
+    validates :end_time, presence: true
+    validates :date, presence: true
+    # validates :description
+    validates :capacity, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+    validates :points, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
     has_many :attendees
 end
