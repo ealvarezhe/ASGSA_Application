aa561ae2ec81fb7a18895b2ded786c3eb9a43e1a
revmya09 <113645178+revmya09@users.noreply.github.com>
2024-02-24 01:48:44 +0000

    successfully able to send welcome emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 
   config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
