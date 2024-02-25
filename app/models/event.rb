3108d76a533f10037069635580bdeacc5fa21fd5
revmya09 <113645178+revmya09@users.noreply.github.com>
2024-02-25 23:19:56 +0000

    changed rsvp button to view event and added validation for time setting for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,16 @@ class Event < ApplicationRecord
     # validates :description
     validates :capacity, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank: true
     validates :points, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-
+    validate :end_time_after_start_time
     has_many :attendees
+
+    private
+
+    def end_time_after_start_time
+        return if end_time.blank? || start_time.blank?
+
+        if end_time <= start_time
+        errors.add(:end_time, "must be after the start time")
+        end
+    end
 end
