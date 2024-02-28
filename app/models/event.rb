2f302e8438ed8cd9a50141e79fa3a75b4e95cb96
Nicholas Nguyen <nick.nguyen1@tamu.edu>
2024-02-28 00:34:20 -0600

    Added a validation field for description

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
     validates :start_time, presence: true
     validates :end_time, presence: true
     validates :date, presence: true
-    # validates :description
+    validates :description, presence: false
     validates :capacity, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank: true
     validates :points, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
