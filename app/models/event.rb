e3c87f74341c3067de19e3716dc7f750ccdc32b4
Nicholas Nguyen <nick.nguyen1@tamu.edu>
2024-02-29 01:54:37 -0600

    Added validation for time (end_time cannot be before start_time)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :search, against: [:member_id], using: { tsearch: { prefix: true } }
     validates :name, presence: true
     validates :location, presence: true
     validates :start_time, presence: true
@@ -9,4 +11,14 @@ class Event < ApplicationRecord
     validates :points, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
     has_many :attendees
+
+    validate :end_time_after_start_time
+
+    private
+
+    def end_time_after_start_time
+        return unless start_time && end_time
+
+        errors.add(:end_time, "must be after the start time") if end_time < start_time
+    end
 end
