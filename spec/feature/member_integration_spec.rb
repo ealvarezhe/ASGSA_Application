6170e2bfccd24b03af082a198d6b64ee9606fd26
ealvarezhe <eduardo_alvz@hotmail.com>
2024-03-04 15:43:51 -0600

    Changed member model role back to member and fixed member integration test to work with it

--- spec/feature/member_integration_spec.rb
+++ spec/feature/member_integration_spec.rb
@@ -4,16 +4,18 @@ RSpec.feature "MemberFeatures", type: :feature do
   before do
     Rails.application.load_seed
 
+    @member1 = create(:member, :admin)
+
     # Setup mock OmniAuth user
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
       provider: 'google_oauth2',
       uid: '123456789',
       info: {
-        email: "john@tamu.edu",
-        first_name: "John",
-        last_name: "Doe",
-        image: "https://example.com/image.jpg"
+        email: @member1.email,
+        first_name: @member1.first_name,
+        last_name: @member1.last_name,
+        image: @member1.avatar_url
       },
       credentials: {
         token: "token",
@@ -21,8 +23,6 @@ RSpec.feature "MemberFeatures", type: :feature do
         expires_at: DateTime.now,
       }
     })
-
-    @member1 = create(:member, :admin)
 
     # Route to trigger the OmniAuth callback directly for testing
     visit member_google_oauth2_omniauth_callback_path
@@ -45,7 +45,15 @@ RSpec.feature "MemberFeatures", type: :feature do
   end
 
   scenario "Update a member's information" do
-    save_page
+    puts "Before update: MemberRoles and their Roles"
+    MemberRole.joins(:role).select('member_roles.member_role_id, member_roles.member_id, roles.name as role_name').each do |mr|
+      puts "MemberRole ID: #{mr.member_role_id}, Member ID: #{mr.member_id}, Role: #{mr.role_name}"
+    end
+
+    # Example query adjusting to use `member_id`
+    Member.select(:member_id, :first_name, :last_name).each do |member|
+      puts "Member ID: #{member.member_id}, Name: #{member.first_name} #{member.last_name}"
+    end
     visit edit_member_path(@member1)
 
     fill_in "member[res_lab]", with: "Lab A"
@@ -61,7 +69,7 @@ RSpec.feature "MemberFeatures", type: :feature do
 
     click_button("Delete this member")
 
-    expect(page).to have_content("Member was successfully deleted.")
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
     expect(page).not_to have_content(@member1.first_name)
   end
 end
